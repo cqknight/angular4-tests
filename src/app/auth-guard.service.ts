c23ba5342ab0da913785d3b71f86dbce8739d8ec
cqknight <cqknight@yahoo.ca>
2017-10-29 23:15:34 -0400

    added Query paras

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -3,7 +3,8 @@ import {
     CanActivate, Router,
     ActivatedRouteSnapshot,
     RouterStateSnapshot,
-    CanActivateChild
+    CanActivateChild,
+    NavigationExtras
 } from '@angular/router';
 import { AuthService } from './auth.service';
 
@@ -27,8 +28,18 @@ export class AuthGuard implements CanActivate, CanActivateChild {
         // Store the attempted URL for redirecting
         this.authService.redirectUrl = url;
 
+        // Create a dummy session id
+        const sessionId = 123456789;
+
+        // Set our navigation extras object
+        // that contains our global query params and fragment
+        const navigationExtras: NavigationExtras = {
+            queryParams: { 'session_id': sessionId },
+            fragment: 'anchor'
+        };
+
         // Navigate to the login page with extras
-        this.router.navigate(['/login']);
+        this.router.navigate(['/login'], navigationExtras);
         return false;
     }
 }
